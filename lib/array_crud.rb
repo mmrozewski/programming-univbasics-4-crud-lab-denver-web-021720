af5017170755695f3c5455d5914b76a03a45b388
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-06 21:41:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  array = []
+  []
 end
 
 def create_an_array
-  array = ["beans," "greens"]
+  array = ["beans," "greens", "queens", "everything in between"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -20,7 +20,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift
+  array.shift 
+  array
 end
 
 def retrieve_element_from_index(array, index_number)
