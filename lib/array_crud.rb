0899e9dd4661ded5c8670c07aafa1c51fe4c3f23
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-06 21:44:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["beans," "greens", "queens", "everything in between"]
+  array = ["beans," "greens", "queens", "everything in between"]
 end
 
 def add_element_to_end_of_array(array, element)
